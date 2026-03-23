ff00ba4a05581a6eb4d6df21a03c83da21ce8a4f
Osama Masoud <osamamasoud07@gmail.com>
2026-03-23 16:57:59 +0700

    Initial Commit

---
+++ ClinicFlow/ClinicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowApp.swift
+//  ClinicFlow
+//
+//  Created by Osama Masoud on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
